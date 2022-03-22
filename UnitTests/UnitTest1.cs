33f7c4ecb7b2266c3c53c093615bacf8ca76a380
Julian Milligan <1904804@student.uwtsd.ac.uk>
2022-03-22 18:22:20 +0000

    1st commit

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World";
+        private const string Expected_option1 = "Option 1";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                unitTesting.Program.Main();
+
+                var results = sw.ToString().Trim();
+                Assert.AreEqual(Expected, results);
+
+            }
+        }
+        [TestMethod]
+        public void TestMethodMenu_Option1()
+        {
+            using (var option1SW = new StringWriter())
+            {
+                Console.SetOut(option1SW);
+                unitTesting.Program.Retun(1);
+
+                var optionResults = option1SW.ToString().Trim();
+                Assert.AreEqual(Expected_option1, optionResults);
+            }
+        }
+    }
+}
---
+++ unitTesting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unitTesting
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public static void Retun(int menuSelection)
+        {
+            switch (menuSelection)
+                {
+
+                case 1:
+                    Console.WriteLine("Option 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Option 2");
+                    break;
+
+
+                }
+        }
+    }
+}
